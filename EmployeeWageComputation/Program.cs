c7880ccc420f939c1df2725e30444a28e890d11c
sahib2442 <kadersahib1234@gmail.com>
2022-08-23 14:56:28 +0530

    [sahib] Add: EmployeeWage

---
+++ EmployeeWageComputation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace EmployeeWageComputation
+{
+    class program
+    {
+        static void main(string[]args)
+        {
+            EmployeeWageComputation a = new EmployeeWageComputation();
+            a.Attendence();
+        }
+    }
+}
