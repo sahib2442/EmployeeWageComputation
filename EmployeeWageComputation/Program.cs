49213a118436d95af1f3f04ba263927211ecdd1c
sahib2442 <kadersahib1234@gmail.com>
2022-08-23 15:14:04 +0530

    [sahib] Add: CalculateEmpWage

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -6,7 +6,7 @@ namespace EmployeeWageComputation
         static void main(string[]args)
         {
             EmployeeWageComputation a = new EmployeeWageComputation();
-            a.Attadence();
+            a.CalculateEmpWage();
         }
     }
 }
